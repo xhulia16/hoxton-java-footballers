ba692cfe662d93654a30a47066dc3f23010ddb57
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-10-06 16:46:58 +0200

    get single player

--- src/main/java/com/footballers/hoxtonjavafootballers/Player.java
+++ src/main/java/com/footballers/hoxtonjavafootballers/Player.java
@@ -1,6 +1,7 @@
 package com.footballers.hoxtonjavafootballers;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +11,8 @@ import javax.persistence.ManyToOne;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,6 +52,16 @@ class PlayerController {
     @GetMapping("/players")
     public List<Player> getAllPlayers() {
         return playerRepo.findAll();
+    }
+
+    @GetMapping("/players/{id}")
+    public ResponseEntity<Player> getSinglePlayer(@PathVariable Integer id) {
+        Optional<Player> match = playerRepo.findById(id);
+        if (match.isPresent()) {
+            return new ResponseEntity<>(match.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
     }
 
     @PostMapping("/teams/{teamId}/players")
